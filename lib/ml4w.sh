44d84b481a5fa480db1a6079999773040303a1d7
millergitca <danmillerca@proton.me>
2026-07-16 09:35:01 -0600

    configure ML4W blur and launcher font

--- lib/ml4w.sh
+++ lib/ml4w.sh
@@ -6,6 +6,8 @@
 
 ML4W_DEFAULT_TERMINAL="ghostty"
 ML4W_DEFAULT_WAYBAR_THEME="/tokyo-night-storm;/tokyo-night-storm"
+ML4W_DEFAULT_BLUR="80x80"
+ML4W_DEFAULT_ROFI_FONT="JetBrainsMono Nerd Font 13"
 
 configure_ml4w() {
   info "Configuring ML4W preferences"
@@ -13,6 +15,8 @@ configure_ml4w() {
   local ml4w_settings_dir="$HOME/.config/ml4w/settings"
   local terminal_file="$ml4w_settings_dir/terminal.sh"
   local waybar_theme_file="$ml4w_settings_dir/waybar-theme.sh"
+  local blur_file="$ml4w_settings_dir/blur.sh"
+  local rofi_font_file="$ml4w_settings_dir/rofi-font.rasi"
 
   if [[ ! -d "$ml4w_settings_dir" ]]; then
     warn "ML4W settings directory not found; skipping ML4W configuration"
@@ -21,7 +25,13 @@ configure_ml4w() {
 
   printf '%s\n' "$ML4W_DEFAULT_TERMINAL" > "$terminal_file"
   printf '%s\n' "$ML4W_DEFAULT_WAYBAR_THEME" > "$waybar_theme_file"
+  printf '%s\n' "$ML4W_DEFAULT_BLUR" > "$blur_file"
+  printf 'configuration { font: "%s"; }\n' \
+  "$ML4W_DEFAULT_ROFI_FONT" \
+  > "$rofi_font_file"
 
   success "ML4W default terminal set to $ML4W_DEFAULT_TERMINAL"
   success "ML4W Waybar theme set to Tokyo Night Storm"
+  success "ML4W blur setting set to $ML4W_DEFAULT_BLUR"
+  success "ML4W Rofi font set to $ML4W_DEFAULT_ROFI_FONT"
 }
