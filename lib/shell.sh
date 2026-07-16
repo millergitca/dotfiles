a5572a75f984dda24cfd0e3b133d47a35a4a111f
millergitca <danmillerca@proton.me>
2026-07-15 18:51:10 -0600

    Improved bootstrap reliability and ML4W compatibility

--- lib/shell.sh
+++ lib/shell.sh
@@ -8,7 +8,22 @@ configure_shell() {
   info "Configuring Zsh"
 
   local zsh_path
+  local ml4w_customization
+  local ml4w_disabled_dir
+  local ml4w_backup
+
   zsh_path="$(command -v zsh)"
+
+  ml4w_customization="$HOME/.config/zshrc/20-customization"
+  ml4w_disabled_dir="$HOME/.config/zshrc-disabled"
+  ml4w_backup="$ml4w_disabled_dir/20-customization.ml4w-disabled"
+
+  if [[ -f "$ml4w_customization" ]]; then
+    info "Disabling conflicting ML4W Zsh customization"
+    mkdir -p "$ml4w_disabled_dir"
+    mw "$ml4w_customization" "$ml4w_backup"
+    success "ML4W Zsh customization disabled"
+  fi
 
   if [[ "$SHELL" != "$zsh_path" ]]; then
     chsh -s "$zsh_path"
